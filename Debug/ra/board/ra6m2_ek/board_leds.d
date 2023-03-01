a8b24bcdc9305c0b1eeee9eae414305c8a9215c6
ferdikurnazdm <ferdikurnazdm@gmail.com>
2023-03-01 08:34:32 +0300

    Led blink upload

---
+++ Debug/ra/board/ra6m2_ek/board_leds.d
@@ -0,0 +1,87 @@
+ra/board/ra6m2_ek/board_leds.o: ../ra/board/ra6m2_ek/board_leds.c \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/fsp_common_api.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/fsp_version.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_gen/bsp_clock_cfg.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_mcu_info.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_elc.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_feature.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_init.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_leds.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_ethernet_phy.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_gen/vector_data.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6M2AF.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_common_api.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_features.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_common_api.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/api/bsp_api.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/fsp_common_api.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/fsp_version.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_gen/bsp_clock_cfg.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_mcu_info.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_elc.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\ra6m2\bsp_feature.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_init.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_leds.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\board\ra6m2_ek\board_ethernet_phy.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_gen/vector_data.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6M2AF.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm4.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_common_api.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_features.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\inc\fsp_common_api.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\ferdi.kurnaz\e2_studio\workspace\blink_deneme_4\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
---
+++ Debug/ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.c \
+../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.c 
+
+C_DEPS += \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.d \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.d 
+
+OBJS += \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.o \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.o 
+
+SREC += \
+Blink_deneme_4.srec 
+
+MAP += \
+Blink_deneme_4.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/%.o: ../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/%.c
+	$(file > $@.in,-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -gdwarf-4 -D_RENESAS_RA_ -D_RA_CORE=CM4 -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/src" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/api" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/fsp/inc/instances" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_gen" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg/bsp" -I"C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
